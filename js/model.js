5fdbf18196d2c1518409dbf763e9b94424a1ae19
Mick Muzac <mmuzac23@gmail.com>
2013-12-08 18:31:12 -0500

    Implemented loadJSON feature, removed key from object

--- js/model.js
+++ js/model.js
@@ -4,54 +4,87 @@ var reflexModel = new function(){
 	nextCondList = ['time', 'total'],
 	entityPropList = ['type', 'min x', 'max x', 'min y', 'max y', 'size', 'duration', 'delay'],
 	actionsList = ['deltax', 'deltay'];
-	
+	self.loadValue = ko.observable("");
 	self.addRound  = function(){
 		
 		self.roundDef.push({entityDef:
-		[
-			{name:"minTargets", val: ko.observable(0)},
-			{name:"maxTargets", val: ko.observable(0)},
-			{name:"minSimultaneous", val: ko.observable(0)},
-			{name:"maxSimultaneous", val: ko.observable(0)},
-			{name:"startTime", val: ko.observable(0)},
-			{name:"forever", val: ko.observable(false)},
-			{name:"interpolate", val: ko.observable(false)},
-			{name:"gravity", val: ko.observable(false)},
-			{name:"entityConditions", val: ko.observableArray([]), options:entityPropList},
-			{name:"actions", val: ko.observableArray([]), options:actionsList},
-			{name:"nextConditions", val: ko.observableArray([]), options:nextCondList}
-		]});
+		{
+			minTargets: {val: ko.observable(0)},
+			maxTargets: {val: ko.observable(0)},
+			minSimultaneous: {val: ko.observable(0)},
+			maxSimultaneous: {val: ko.observable(0)},
+			startTime: {val: ko.observable(0)},
+			forever: {val: ko.observable(false)},
+			interpolate: {val: ko.observable(false)},
+			gravity: {val: ko.observable(false)},
+			entityConditions: {val: ko.observableArray([]), options:entityPropList},
+			actions: {val: ko.observableArray([]), options:actionsList},
+			nextConditions: {val: ko.observableArray([]), options:nextCondList}
+		}});
 	};
 	self.roundDef  = ko.observableArray([]);	
 	self.addNewCondition = function(obj){
 		//arr.push({metric:"",value:0});
-		console.log(name);
-		switch(obj.name){
+		console.log(obj);
+		
+		/*
+		 * This isn't terribly stable. Find another way to determine array type 
+		 */
+		switch(obj.options[0]){
 			
-			case "actions": 
+			case "deltax": 
 				obj.val.push({metric:"deltax",value:0});
 				break;
-			case "entityConditions": 
+			case "type": 
 				obj.val.push({metric:"min x",value:0});
 				break;
-			case "nextConditions":
+			case "time":
 				obj.val.push({metric:"",value:0});
 				break;
 		}
 	};
 	self.makeJSON = function(){
-		var tempJS = ko.toJS(self.roundDef);
-		var outObj  = {roundDef:[]};
+		var roundDef = ko.toJS(self.roundDef);
 		
-		for(var i = 0; i < tempJS.length; i++){
-			outObj.roundDef.push({entityDef:{}});
-			for(var g = 0; g < tempJS[i]["entityDef"].length;g++){
-				outObj.roundDef[i].entityDef[tempJS[i]["entityDef"][g].name] = tempJS[i]["entityDef"][g].val;
+		for(var i = 0; i < roundDef.length; i++){
+			for(var key in roundDef[i].entityDef){
+				roundDef[i].entityDef[key] = roundDef[i].entityDef[key].val;
 			}
 		}
 		
-		console.log(ko.toJSON(outObj));
-	}
+		console.log(ko.toJSON({roundDef:roundDef}));
+	};
+
+	self.loadJSON = function(str){
+		var roundDef;
+		try{
+			//Change later once other properties are added..
+			if(typeof str == "string")
+				roundDef = JSON.parse(str).roundDef;
+				
+			else {
+				roundDef = JSON.parse(str.loadValue()).roundDef;
+				str.loadValue("");
+			}
+		}
+		catch(e){
+			console.error(e);
+			return;
+		}
+		
+		self.roundDef.removeAll();
+
+		for(var i = 0; i < roundDef.length; i++){
+			
+			self.addRound();
+			for(var key in roundDef[i].entityDef){
+			
+				self.roundDef()[i].entityDef[key].val(roundDef[i].entityDef[key]);
+			}
+		}
+		
+		console.log(ko.toJSON(roundDef));
+	};
 };
 
 ko.applyBindings(reflexModel);
